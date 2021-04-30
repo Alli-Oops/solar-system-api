9822cf3d1d3a6d389fac7a6cdd686a18af694127
Alli-Oops <AllisonLeonardSeattle@gmail.com>
2021-04-30 10:42:37 -0700

    GTeamwork and Pair Programming with Erika and Al solar_system_development project

---
+++ app/models/planet.py
@@ -0,0 +1,10 @@
+from app import db
+
+# db = solar_system_development?
+
+class Planet(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    color = db.Column(db.String)
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,18 @@
 from flask import Blueprint
+from app.models.planet import Planet
+from flask import request, Blueprint, make_response
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_planet = Planet(title=request_body["name"],
+                    description=request_body["description"],
+                    color=request)_body["color"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
