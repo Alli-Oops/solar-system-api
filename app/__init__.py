45499d28cd268925c97db18b0cce0e0ae3544ae0
Alli-Oops <AllisonLeonardSeattle@gmail.com>
2021-05-02 12:41:28 -0700

    updated routes.py and __init__.py to define endpoints and handle migration. Made sure to run flask db -migrate with initial migration message AND run flask db upgrade with any changes

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db = SQLAlchemy() 
+db = SQLAlchemy() # 
 migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+## 
 
-    db.init_app(app)
+
+    db.init_app(app) # allows SQLAlchemy to talk to the database
     migrate.init_app(app, db)
-    
+
+
+    from .routes import solar_world_development_bp # new line that might fix 404?
+    app.register_blueprint(solar_world_development_bp) # new line that might fix 404?
+    # use app's pre-defined function register_blueprint() to register the solar_world_development_bp Blueprint.
+
 
     return app
 
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,39 @@
 from flask import Blueprint
 from app.models.planet import Planet
-from flask import request, Blueprint, make_response
+from flask import request, Blueprint, make_response, jsonify
+from app import db
 
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+# planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+solar_world_development_bp = Blueprint("Planet", __name__, url_prefix="/planets")
 
+# @app.route("/", methods=["GET"])
+# def test():
+#     print("hello world")
 
-@planets_bp.route("", methods=["POST"])
-def handle_books():
+# ENDPOINT 1 # Together these arguments ("/create-a-planet", methods=["POST"]) define what type of request will be routed to this function. The first argument defines the path (or URL) of the request and the second argument defines a list of HTTP methods (i.e. GET, PUT, POST) the request could have.
+# 
+@solar_world_development_bp.route("/create-a-planet", methods=["POST"])
+def create_a_planet():
     request_body = request.get_json()
     new_planet = Planet(title=request_body["name"],
                     description=request_body["description"],
-                    color=request)_body["color"])
+                    color=request_body["color"])
 
     db.session.add(new_planet)
     db.session.commit()
 
     return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+# ENDPOINT 2
+@solar_world_development_bp.route("/retrieve-all-planets", methods=["GET"]) # 404 in Postman
+def retrieve_all_planets():
+    planets = Planet.query.all() 
+    return jsonify(planets)
+
+    return jsonify(p.to_dict() for p in planets)
+
+# ENDPOINT 3 # jsonify - flask handlesI
+@solar_world_development_bp.route("/retrieve-one-planet/<planet_title>", methods=["GET"])
+def retrieve_one_planets(planet_title):
+    planet = Planet.query.filter_by(title=planet_title).first()
+    return jsonify(planet)
